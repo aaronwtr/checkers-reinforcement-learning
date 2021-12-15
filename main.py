11c5e51dab75ff8e60ab5b1c3da235411b83872f
Aaron <aaronwenteler@gmail.com>
2021-12-15 22:01:59 +0100

    Define constants and a blank 8x8 checkers board

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 import pygame
+from utils.constants import *
+
+FPS = 60
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Checkers")
+
+
+def main():
+    """
+    Main function of the program
+    """
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.KEYDOWN:
+                pass
+
+    pygame.quit()
+
+
+main()
 
---
+++ utils/constants.py
@@ -0,0 +1,11 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+NUM_ROWS, NUM_COLS = 8, 8
+SQUARE_SIZE = WIDTH // NUM_COLS
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
